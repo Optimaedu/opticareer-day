b792353ba6cd3e275435ca4ab751d2e957da83dd
Jimi Ahlgren <jimi.ahlgren@gmail.com>
2023-01-11 20:43:27 +0200

    api hello testing

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,11 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { DefaultEntityManager, withMikroOrm } from '../../core/mikro-orm/server'
 
-export default withMikroOrm(async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  em: DefaultEntityManager
-) => {
-  res.status(200).json({ message: 'Hello' })
-});
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ message: 'Hello' });
+}
+
+export default handler;
